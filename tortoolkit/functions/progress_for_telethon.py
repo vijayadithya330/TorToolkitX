3b6432a6dffcfb65587005332f60a34d8ba88917
vijayadithya330 <88371471+vijayadithya330@users.noreply.github.com>
2021-09-17 10:56:37 +0530

    Update progress_for_telethon.py

--- tortoolkit/functions/progress_for_telethon.py
+++ tortoolkit/functions/progress_for_telethon.py
@@ -38,7 +38,7 @@ async def progress(
             seconds=estimated_total_time / 1000
         )
 
-        progress = "🎳[{0}{1}] \n= {2}%\n".format(
+        progress = "🎳[{0}{1}] = {2}%\n".format(
             "".join(
                 [get_val("COMPLETED_STR") for i in range(math.floor(percentage / 10))]
             ),
@@ -51,7 +51,7 @@ async def progress(
             round(percentage, 2),
         )
 
-        tmp = progress + "{0} of {1}\n⏱Speed: {2}/s\n🕒ETA: {3}\n📡Using engine: Telethon".format(
+        tmp = progress + "👀Status: {0} of {1}\n⏱Speed: {2}/s\n🕒ETA: {3}\n📡Using Engine: Telethon".format(
             human_readable_bytes(current),
             human_readable_bytes(total),
             human_readable_bytes(speed),
@@ -61,11 +61,11 @@ async def progress(
         try:
             if not message.photo:
                 await message.edit(
-                    text="**📤__ᴜᴘʟᴏᴀᴅɪɴɢ...__** `🗂{}`\n\n{}".format(file_name, tmp)
+                    text="**📤__ᴜᴘʟᴏᴀᴅɪɴɢ...__** `\n\n🗂{}`\n{}".format(file_name, tmp)
                 )
             else:
                 await message.edit(
-                    caption="**📤__ᴜᴘʟᴏᴀᴅɪɴɢ...__** `🗂{}`\n\n{}".format(file_name, tmp)
+                    caption="**📤__ᴜᴘʟᴏᴀᴅɪɴɢ...__** `\n\n🗂{}`\n{}".format(file_name, tmp)
                 )
         except Exception as e:
             logging.error(e)
