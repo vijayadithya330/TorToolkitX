7cbf2fd26560f39de7251d71d1a1ada4c37f7822
vijayadithya330 <88371471+vijayadithya330@users.noreply.github.com>
2021-11-19 15:41:42 +0530

    Update progress_for_pyrogram.py

--- tortoolkit/functions/progress_for_pyrogram.py
+++ tortoolkit/functions/progress_for_pyrogram.py
@@ -53,7 +53,7 @@ async def progress_for_pyrogram(
         elapsed_time = human_readable_timedelta(elapsed_time)
         estimated_total_time = human_readable_timedelta(estimated_total_time)
 
-        progress = "<b>🎳[{0}{1}] = {2}%</b>\n".format(
+        progress = "🎳[{0}{1}] = <b>{2}%</b>\n".format(
             "".join(
                 [get_val("COMPLETED_STR") for _ in range(math.floor(percentage / 10))]
             ),
